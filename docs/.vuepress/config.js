db99357e2a1e25bb4223c65ff466648da9524f3c
xiuhonglee <xiuhong@mogujie.com>
2018-08-14 09:48:55 +0800

    add mysql

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     base: '/',
-    title: '互动游戏',
-    description: 'Canvas webGL CSS3',
+    title: '前端开发',
+    description: '学习实践总结',
     markdown: {
         lineNumbers: true
     },
@@ -33,15 +33,20 @@ module.exports = {
                 link: '/CSS3/c02'
             },
             {
-                text: 'nginx',
+                text: 'Nginx',
                 link: '/Nginx/n01'
+            },
+            {
+                text: 'MySQL',
+                link: '/MySQL/m01'
             }
         ],
         sidebar: {
             '/Canvas_ch01/': ['d01', 'd02', 'd03', 'd04', 'd05', 'd06', 'd07', 'd08', 'd09', 'd10'],
             '/Canvas_ch02/': ['t01', 't02', 't03'],
             '/CSS3/': ['c02', 'c03', 'c04', 'c05'],
-            '/Nginx/': ['n01', 'n02', 'n03', 'n04']
+            '/Nginx/': ['n01', 'n02', 'n03', 'n04', 'n05'],
+            '/MySQL/': ['m01', 'm02']
         }
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -4,11 +4,23 @@ heroImage: https://s10.mogucdn.com/mlcdn/c45406/180518_8dl5d16c29i1lia73dklab1cl
 actionText: Cancas →
 actionLink: /Canvas_ch01/
 features:
+- title: 计算机网络
+  details: 基础概念，协议详解 
+- title: 操作系统相关
+  details: 计算机网络相关 
+- title: Node
+  details: Node底层相关
+- title: Nginx
+  details: Nginx基本概念，基本操作，常见问题
+- title: MySQL
+  details: MySQL基本概念，基本操作，常见问题
 - title: Canvas
   details: Cancas基本绘图操作
 - title: webGL
-  details: webGL 
+  details: webGL基本概念
 - title: CSS3
   details: CSS3D变换原理介绍，绘制正N边形以及绘制正N面体
+- title: 工具相关
+  details: wireshark 
 footer: MIT Licensed | Copyright © 2018-present Evan You
 ---
